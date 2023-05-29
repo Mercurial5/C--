4dc95b4a407d97accc08a7113d4ae2df4741d352
Mercurial5 <dias.nespayev@gmail.com>
2023-05-29 18:23:36 +0600

    feat: add TextSpan to Token class

--- C--/Token.h
+++ C--/Token.h
@@ -5,12 +5,15 @@
 
 #include "TokenType.h"
 
+#include "TextSpan.h"
+
 class Token {
 public:
 	TokenType type;
 	int position;
 	std::string raw;
 	std::any value;
+	TextSpan span;
 
 	Token(TokenType, int, std::string, std::any);
 	Token(TokenType, int, char, std::any);
