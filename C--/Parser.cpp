61ddfc975b7759a8573cd6090769715ad9d4b756
Mercurial5 <dias.nespayev@gmail.com>
2023-05-31 16:55:20 +0600

    feat: parse `NameExpression` ad `AssignmentExpression`

--- C--/Parser.cpp
+++ C--/Parser.cpp
@@ -10,6 +10,8 @@
 #include "UnaryExpression.h"
 #include "BinaryExpression.h"
 #include "ParenthesizedExpression.h"
+#include "NameExpression.h"
+#include "AssignmentExpression.h"
 #include "BadExpression.h"
 
 #include "Lexer.h"
@@ -35,20 +37,34 @@ std::shared_ptr<ExpressionTree> Parser::parse() {
 	return std::make_shared<ExpressionTree>(this->diagnostics, root, std::make_shared<Token>(end_of_file_token));
 }
 
-std::shared_ptr<Expression> Parser::parse_expression(int parent_precedence) {
+std::shared_ptr<Expression> Parser::parse_expression() {
+	return this->parse_assignment_expression();
+}
+
+std::shared_ptr<Expression> Parser::parse_assignment_expression() {
+	if (this->peek().type == IdentifierToken && this->peek(1).type == EqualToken) {
+		std::shared_ptr<Token> identifier_token = std::make_shared<Token>(this->next());
+		std::shared_ptr<Token> equal_token = std::make_shared<Token>(this->next());
+		std::shared_ptr<Expression> expression = this->parse_expression();
+		return std::make_shared<AssignmentExpression>(identifier_token, equal_token, expression);
+	}
+
+	return this->parse_binary_expression();
+}
+
+std::shared_ptr<Expression> Parser::parse_binary_expression(int parent_precedence) {
 	int unary_operator_precedence = ParserRules::get_unary_operator_precedence(this->current().type);
 
 	std::shared_ptr<Expression> left;
 	if (unary_operator_precedence >= parent_precedence && unary_operator_precedence != 0) {
 		std::shared_ptr<Token> operator_token = std::make_shared<Token>(this->next());
-		std::shared_ptr<Expression> expression = this->parse_expression(unary_operator_precedence);
+		std::shared_ptr<Expression> expression = this->parse_binary_expression(unary_operator_precedence);
 		std::shared_ptr<Expression> unary_expression(new UnaryExpression(operator_token, expression));
 		left = unary_expression;
 	}
 	else {
 		left = this->parse_primary();
 	}
-
 
 	while (true) {
 		int precedence = ParserRules::get_binary_operator_precedence(this->current().type);
@@ -58,7 +74,7 @@ std::shared_ptr<Expression> Parser::parse_expression(int parent_precedence) {
 		}
 
 		std::shared_ptr<Token> operator_token = std::make_shared<Token>(this->next());
-		std::shared_ptr<Expression> right = this->parse_expression(precedence);
+		std::shared_ptr<Expression> right = this->parse_binary_expression(precedence);
 
 		std::shared_ptr<Expression> binary_expression(new BinaryExpression(left, operator_token, right));
 		left = binary_expression;
@@ -79,12 +95,24 @@ std::shared_ptr<Expression> Parser::parse_primary() {
 		Token close = this->match(CloseParenthesisToken);
 		return std::make_shared<ParenthesizedExpression>(std::make_shared<Token>(open), expression, std::make_shared<Token>(close));
 	}
-
+	case IdentifierToken: {
+		std::shared_ptr<Token> identifier_token = std::make_shared<Token>(this->next());
+		return std::make_shared<NameExpression>(identifier_token);
+	}
 	default: {
-		Token current = this->match(NumberToken);
-		return std::make_shared<LiteralExpression>(std::make_shared<Token>(current));
+		Token identifier_token = this->match(NumberToken);
+		return std::make_shared<LiteralExpression>(std::make_shared<Token>(identifier_token));
 	}
 	}
+}
+
+Token Parser::peek(int offset) {
+	int index = this->position + offset;
+	if (this->position >= this->tokens.size()) {
+		return *this->tokens[this->tokens.size() - 1];
+	}
+
+	return *this->tokens[index];
 }
 
 Token Parser::match(TokenType expression_type) {
