3d9150cacec0b43ef1094cfc3db00c3af63d7874
Mercurial5 <dias.nespayev@gmail.com>
2023-05-31 17:35:58 +0600

    feat: add `bind_name_expression` and `bind_assignment_expression`

--- C--/Binder.h
+++ C--/Binder.h
@@ -1,10 +1,12 @@
 #pragma once
 
+#include <typeinfo>
+#include <optional>
 #include <memory>
 #include <vector>
 #include <string>
-#include <typeinfo>
-#include <optional>
+#include <map>
+#include <any>
 
 #include "BoundExpression.h"
 #include "BoundUnaryOperatorType.h"
@@ -16,6 +18,8 @@
 #include "UnaryExpression.h"
 #include "BinaryExpression.h"
 #include "ParenthesizedExpression.h"
+#include "NameExpression.h"
+#include "AssignmentExpression.h"
 
 #include "Token.h"
 
@@ -25,14 +29,18 @@ class Binder {
 public:
 	DiagnosticBag diagnostics;
 	std::shared_ptr<BoundExpression> bind(std::shared_ptr<Expression>);
-	Binder(DiagnosticBag);
+	Binder(DiagnosticBag, std::map<std::string, std::any>&);
 
 private:
+	std::map<std::string, std::any>* variables;
+
 	std::shared_ptr<BoundExpression> bind_expression(std::shared_ptr<Expression>);
 	std::shared_ptr<BoundExpression> bind_literal_expression(std::shared_ptr<LiteralExpression>);
 	std::shared_ptr<BoundExpression> bind_unary_expression(std::shared_ptr<UnaryExpression>);
 	std::shared_ptr<BoundExpression> bind_binary_expression(std::shared_ptr<BinaryExpression>);
 	std::shared_ptr<BoundExpression> bind_parenthesized_expression(std::shared_ptr<ParenthesizedExpression>);
+	std::shared_ptr<BoundExpression> bind_name_expression(std::shared_ptr<NameExpression>);
+	std::shared_ptr<BoundExpression> bind_assignment_expression(std::shared_ptr<AssignmentExpression>);
 	
 	template <typename CastTo, typename CastFrom>
 	std::shared_ptr<CastTo> cast(std::shared_ptr<CastFrom> obj) {
